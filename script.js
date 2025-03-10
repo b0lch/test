1e06b23f24ed98a3c13371324b699fe1cee69a9f
b0lch <minecrafter027@outlook.de>
2025-03-10 19:13:15 +0100

    Update script.js

--- script.js
+++ script.js
@@ -1,8 +1,2 @@
 document.addEventListener("DOMContentLoaded", function() {
     let popup = document.getElementById("popup");
-
-    // Zeigt das Popup nach 2 Sekunden an
-    setTimeout(() => {
-        popup.style.display = "block";
-    }, 2000);
-});
